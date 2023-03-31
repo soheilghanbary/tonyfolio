5215ab37b3cef354c8cf19d9cb0f385bee522f6c
soheilghanbary <soli.ghanbary@gmail.com>
2023-03-31 17:26:34 +0430

    add Light Theme

--- src/components/modules/navbar.tsx
+++ src/components/modules/navbar.tsx
@@ -43,7 +43,14 @@ function Settings() {
   const { theme, setTheme } = useTheme();
   const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
 
-  return <Button onClick={toggleTheme} color="warning" text="Settings" icon={<CogsIcon />} />;
+  return (
+    <Button
+      onClick={toggleTheme}
+      color="warning"
+      text="Theme"
+      icon={<MoonIcon />}
+    />
+  );
 }
 
 const CogsIcon = () => (
@@ -64,6 +71,23 @@ const CogsIcon = () => (
       strokeLinecap="round"
       strokeLinejoin="round"
       d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+    />
+  </svg>
+);
+
+const MoonIcon = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-6 h-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
     />
   </svg>
 );
